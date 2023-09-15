f1776c3bdd20252b28693bf68f7c8e12f7901f70
Inhee <whdlsgml1599@gmail.com>
2023-09-15 20:10:13 +0900

    Changes by 정강용

---
+++ src/main/java/com/example/ali/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.ali.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String username;
+    private String password;
+    private String email;
+    private Double point;
+    private UserRoleEnum role;
+
+
+    // getters and setters
+}
