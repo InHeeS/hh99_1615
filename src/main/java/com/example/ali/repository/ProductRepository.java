00f8dbf69f57b4e1effc238a57a6f0b4fd84616a
Inhee <whdlsgml1599@gmail.com>
2023-09-16 14:58:54 +0900

    ..

--- src/main/java/com/example/ali/repository/ProductRepository.java
+++ src/main/java/com/example/ali/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.example.ali.repository;
 
 import com.example.ali.entity.Product;
+import com.example.ali.entity.Store;
 import com.example.ali.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    Page<Product> findAllByStore(Store store);
 
 }
