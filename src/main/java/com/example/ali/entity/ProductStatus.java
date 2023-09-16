4c91b5793c398a46103c3b078f5e56c60863949b
heypop <hey@herenas.com>
2023-09-16 11:52:51 +0900

    Product Entity -> Status 컬럼 및 기본적인 클래스 생성 진행

---
+++ src/main/java/com/example/ali/entity/ProductStatus.java
@@ -0,0 +1,15 @@
+package com.example.ali.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum ProductStatus {
+    // 판매중
+    AVAILABLE,
+
+    // 일시품절
+    OUT_OF_STOCK,
+
+    // 품절
+    DISCONTINUED
+}
---
+++ src/main/java/com/example/ali/service/ProductService.java
@@ -0,0 +1,41 @@
+package com.example.ali.service;
+
+import com.example.ali.dto.ProductResponseDto;
+import com.example.ali.entity.Product;
+import com.example.ali.entity.User;
+import com.example.ali.entity.UserRoleEnum;
+import com.example.ali.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    // 전체 조회
+    @Transactional(readOnly = true)
+    public Page<ProductResponseDto> getProducts(int page, int size, String sortBy, boolean isAsc) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<Product> productList = productRepository.findAll(pageable);
+        return productList.map(ProductResponseDto::new);
+    }
+
+    // 게시물 id로 조회
+    public ProductResponseDto getProduct(Long productId) {
+        Product product = productRepository.findById(productId).orElseThrow(
+                () -> new IllegalArgumentException("해당 상품을 찾을 수 없습니다.")
+        );
+        return new ProductResponseDto(product);
+    }
+
+
+}
