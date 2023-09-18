2a6025daf969379dd0be36a9267c219809e0d549
heypop <hey@herenas.com>
2023-09-18 10:59:14 +0900

    aws DB 연결, DB_PASSWORD 환경변수 처리

--- src/main/java/com/example/ali/repository/ProductRepository.java
+++ src/main/java/com/example/ali/repository/ProductRepository.java
@@ -2,11 +2,7 @@ package com.example.ali.repository;
 
 import com.example.ali.entity.Product;
 import com.example.ali.entity.Store;
-import com.example.ali.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
--- src/main/java/com/example/ali/repository/StoreRepository.java
+++ src/main/java/com/example/ali/repository/StoreRepository.java
@@ -1,8 +1,9 @@
 package com.example.ali.repository;
 
 import com.example.ali.entity.Store;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
